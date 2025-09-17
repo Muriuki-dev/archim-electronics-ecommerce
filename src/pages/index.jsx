66420eac220b32ea13eccf3807208725b9d98fd5
Muriuki-dev <youjokealot@gmail.com>
2025-09-17 21:32:07 +0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -27,9 +27,9 @@ export default function Home() {
     
     <HomeHeroSlider/>
     <ProductArea/>
-    {/* <NewArrivals/> */}
-    {/* <FeatureArea/> */}
-    {/* <CtaArea/> */}
+    <NewArrivals/>
+    <FeatureArea/>
+    <CtaArea/>
     <Footer/>
   </Wrapper>
 )
