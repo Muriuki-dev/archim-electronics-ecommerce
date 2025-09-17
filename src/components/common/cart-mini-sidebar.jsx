01e9a44c25d6db9bf8026cd9a8159355bd934e3e
Muriuki-dev <youjokealot@gmail.com>
2025-09-17 20:26:54 +0300

    Update cart-mini-sidebar.jsx

--- src/components/common/cart-mini-sidebar.jsx
+++ src/components/common/cart-mini-sidebar.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
 // internal
 import useCartInfo from '@/hooks/use-cart-info';
-import RenderCartProgress from './render-cart-progress';
+
 import empty_cart_img from '@assets/img/product/cartmini/empty-cart.png';
 import { closeCartMini, remove_product } from '@/redux/features/cartSlice';
 
@@ -29,7 +29,7 @@ const handleCloseCartMini = () => {
           <div className="cartmini__top-wrapper">
             <div className="cartmini__top p-relative">
               <div className="cartmini__top-title">
-                <h4>Shopping cart</h4>
+                <h4>Popote Shopping cart</h4>
               </div>
               <div className="cartmini__close">
                 <button onClick={() => dispatch(closeCartMini())} type="button" className="cartmini__close-btn cartmini-close-btn">
@@ -37,22 +37,23 @@ const handleCloseCartMini = () => {
                 </button>
               </div>
             </div>
-            <div className="cartmini__shipping">
-              <RenderCartProgress/>
-            </div>
+            
             {cart_products.length > 0 && <div className="cartmini__widget">
               {cart_products.map((item) => (
                 <div key={item._id} className="cartmini__widget-item">
                   <div className="cartmini__thumb">
-                    <Link href={`/product-details/${item._id}`}>
+                    <Link href={`/product-details/Ksh{item._id}`}>
                       <Image src={item.img} width={70} height={60} alt="product img" />
                     </Link>
                   </div>
                   <div className="cartmini__content">
                     <h5 className="cartmini__title">
-                      <Link href={`/product-details/${item._id}`}>{item.title}</Link>
+                      <Link href={`/product-details/Ksh{item._id}`}>{item.title}</Link>
                     </h5>
-                    
+                    <div className="cartmini__price-wrapper">
+                      {item.discount > 0 ? <span className="cartmini__price">Ksh{(Number(item.price) - (Number(item.price) * Number(item.discount)) / 100).toFixed(2)}</span> : <span className="cartmini__price">Ksh{item.price.toFixed(2)}</span>}
+                      <span className="cartmini__quantity">{" "}x{item.orderQuantity}</span>
+                    </div>
                   </div>
                   <a onClick={() => handleRemovePrd({ title: item.title, id: item._id })} className="cartmini__del cursor-pointer"><i className="fa-regular fa-xmark"></i></a>
                 </div>
@@ -62,17 +63,17 @@ const handleCloseCartMini = () => {
             {cart_products.length === 0 && <div className="cartmini__empty text-center">
               <Image src={empty_cart_img} alt="empty-cart-img" />
               <p>Your Cart is empty</p>
-              <Link href="/shop" className="tp-btn">Go to Shop</Link>
+             
             </div>}
           </div>
           <div className="cartmini__checkout">
             <div className="cartmini__checkout-title mb-30">
               <h4>Subtotal:</h4>
-              <span>${total.toFixed(2)}</span>
+              <span>Ksh{total.toFixed(2)}</span>
             </div>
             <div className="cartmini__checkout-btn">
-              <Link href="/cart" onClick={handleCloseCartMini} className="tp-btn mb-10 w-100"> view cart</Link>
-              <Link href="/checkout" onClick={handleCloseCartMini} className="tp-btn tp-btn-border w-100"> checkout</Link>
+              
+              <Link href="/checkout" onClick={handleCloseCartMini} className="tp-btn tp-btn-border w-100">Proceed to Order</Link>
             </div>
           </div>
         </div>
