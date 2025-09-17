d1a8f4757675c66054f85b4f80c8eae80e0dfbf5
Muriuki-dev <youjokealot@gmail.com>
2025-09-17 16:54:24 +0300

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,162 @@
+export const products = [
+  {
+    id: 1,
+    name: "Cat5e RJ45 Ethernet Cable 20m",
+    type: "Cable",
+    price: 1000,
+    image: "https://unsplash.com/photos/two-black-cables-coiled-on-a-yellow-background-JfDcyqjedSg/download?force=true",
+    tags: ["new"]
+  },
+  {
+    id: 2,
+    name: "Cat6 Ethernet Patch Cord 2m",
+    type: "Cable",
+    price: 400,
+    image: "https://unsplash.com/photos/blue-optical-fiber-cables-inserted-into-ports-of-switch-panel-inside-server-rack-pamzgvziyJE/download?force=true",
+    tags: ["new", "featured"]
+  },
+  {
+    id: 3,
+    name: "Veto 1 Gang 1 Way Plastic Switch",
+    type: "Switch",
+    price: 30,
+    image: "https://unsplash.com/photos/black-computer-power-supply-unit-with-cables-E2aPGg02QdI/download?force=true",
+    tags: ["featured"]
+  },
+  {
+    id: 4,
+    name: "Veto 2 Gang 1 Way Smooth Gold Switch",
+    type: "Switch",
+    price: 370,
+    image: "https://unsplash.com/photos/two-black-cables-coiled-on-a-yellow-background-JfDcyqjedSg/download?force=true",
+    tags: ["topSellers"]
+  },
+  {
+    id: 5,
+    name: "E27 LED Bulb 12W Warm Light",
+    type: "Bulb",
+    price: 160,
+    image: "https://unsplash.com/photos/green-circuit-board-raLeFIxXgDY/download?force=true",
+    tags: ["new", "topSellers"]
+  },
+  {
+    id: 6,
+    name: "E27 LED Bulb 5W Daylight",
+    type: "Bulb",
+    price: 80,
+    image: "https://unsplash.com/photos/blue-optical-fiber-cables-inserted-into-ports-of-switch-panel-inside-server-rack-pamzgvziyJE/download?force=true",
+    tags: ["featured"]
+  },
+  {
+    id: 7,
+    name: "Ceiling Light 36W Square Panel",
+    type: "Light Fixture",
+    price: 2500,
+    image: "https://unsplash.com/photos/black-computer-power-supply-unit-with-cables-E2aPGg02QdI/download?force=true",
+    tags: ["topSellers"]
+  },
+  {
+    id: 8,
+    name: "Power Extension Cord 4 Way 3m",
+    type: "Power Accessory",
+    price: 500,
+    image: "https://unsplash.com/photos/green-circuit-board-raLeFIxXgDY/download?force=true",
+    tags: ["new"]
+  },
+  {
+    id: 9,
+    name: "Travel Adapter",
+    type: "Power Accessory",
+    price: 70,
+    image: "https://unsplash.com/photos/blue-optical-fiber-cables-inserted-into-ports-of-switch-panel-inside-server-rack-pamzgvziyJE/download?force=true",
+    tags: ["topSellers"]
+  },
+  {
+    id: 10,
+    name: "USB-C Power Adapter 12V 24W",
+    type: "Adapter",
+    price: 500,
+    image: "https://unsplash.com/photos/black-computer-power-supply-unit-with-cables-E2aPGg02QdI/download?force=true",
+    tags: ["featured"]
+  },
+  {
+    id: 11,
+    name: "TP-Link TL-WR941N Wireless Router",
+    type: "Router",
+    price: 4800,
+    image: "https://unsplash.com/photos/blue-optical-fiber-cables-inserted-into-ports-of-switch-panel-inside-server-rack-pamzgvziyJE/download?force=true",
+    tags: ["topSellers", "featured"]
+  },
+  {
+    id: 12,
+    name: "TP-Link 8-Port Desktop Switch (10/100Mbps)",
+    type: "Switch / Networking",
+    price: 1500,
+    image: "https://unsplash.com/photos/two-black-cables-coiled-on-a-yellow-background-JfDcyqjedSg/download?force=true",
+    tags: ["new"]
+  },
+  {
+    id: 13,
+    name: "Telephone Extension Cable 4 Core",
+    type: "Cable",
+    price: 220,
+    image: "https://unsplash.com/photos/black-computer-power-supply-unit-with-cables-E2aPGg02QdI/download?force=true",
+    tags: ["featured"]
+  },
+  {
+    id: 14,
+    name: "Coaxial RG6 Cable 300m Roll",
+    type: "Cable",
+    price: 6000,
+    image: "https://unsplash.com/photos/blue-optical-fiber-cables-inserted-into-ports-of-switch-panel-inside-server-rack-pamzgvziyJE/download?force=true",
+    tags: ["topSellers"]
+  },
+  {
+    id: 15,
+    name: "Light Switch Dimmer 500W",
+    type: "Switch / Dimmer",
+    price: 820,
+    image: "https://unsplash.com/photos/two-black-cables-coiled-on-a-yellow-background-JfDcyqjedSg/download?force=true",
+    tags: ["new"]
+  },
+  {
+    id: 16,
+    name: "USB to RJ45 Ethernet Adapter",
+    type: "Adapter",
+    price: 1200,
+    image: "https://unsplash.com/photos/green-circuit-board-raLeFIxXgDY/download?force=true",
+    tags: ["featured"]
+  },
+  {
+    id: 17,
+    name: "18650 Lithium Rechargeable Battery",
+    type: "Battery",
+    price: 200,
+    image: "https://unsplash.com/photos/black-computer-power-supply-unit-with-cables-E2aPGg02QdI/download?force=true",
+    tags: ["new", "topSellers"]
+  },
+  {
+    id: 18,
+    name: "47 inch LED TV Panel",
+    type: "Display / Screen",
+    price: 40000,
+    image: "https://unsplash.com/photos/blue-optical-fiber-cables-inserted-into-ports-of-switch-panel-inside-server-rack-pamzgvziyJE/download?force=true",
+    tags: ["topSellers"]
+  },
+  {
+    id: 19,
+    name: "Solar Flood Light 100W IP66",
+    type: "Light / Outdoor",
+    price: 4500,
+    image: "https://unsplash.com/photos/two-black-cables-coiled-on-a-yellow-background-JfDcyqjedSg/download?force=true",
+    tags: ["featured", "new"]
+  },
+  {
+    id: 20,
+    name: "Network Cable Tester (RJ45 / RJ11)",
+    type: "Tool / Accessory",
+    price: 650,
+    image: "https://unsplash.com/photos/green-circuit-board-raLeFIxXgDY/download?force=true",
+    tags: ["topSellers"]
+  }
+];
